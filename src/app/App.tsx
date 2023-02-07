f89f487717ce7c2d3abc98e0070bf2c33f8c3d5c
Arslan <arseqpage@gmail.com>
2023-02-07 20:18:25 +0300

    add fsd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Route, Routes, Link} from 'react-router-dom';
-import {Counter} from "./components/Counter";
 import './styles/index.scss'
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {classNames} from "shared/lib/classNames/classNames";
 
 const App = () => {
     const { theme, handleChangeTheme} = useTheme();
@@ -22,8 +20,8 @@ const App = () => {
 
             <Suspense fallback={<div>loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
