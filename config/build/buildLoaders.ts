cf148df67a5360c90f14e9b18e39420fd90cd899
Arslan <arseqpage@gmail.com>
2023-02-11 13:56:47 +0300

    17 Add babel and i18n extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,7 +7,26 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-    }
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract", {
+                            "locales": ['ru', 'en'],
+                            "keyAsDefaultValue": true,
+                        }
+                    ]
+                ]
+            }
+        }
+    };
 
     const cssLoaders = {
             test: /\.s[ac]ss$/i,
@@ -48,6 +67,7 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typeScriptLoader,
         cssLoaders,
     ]
