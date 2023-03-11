99917f8aceb8391465cc6b5eee3235659a9b6c9e
Arslan <arseqpage@gmail.com>
2023-03-11 15:49:03 +0300

    26 Fixes another one

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React, { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import LangSwitcher from 'shared/ui/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +12,7 @@ interface SidebarProps {
 
 const Sidebar = ({ className = '' }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((collapsed) => !collapsed);
@@ -22,13 +25,13 @@ const Sidebar = ({ className = '' }: SidebarProps) => {
             }
             data-testid="sidebar"
         >
-            {/* <button */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    type="button" */}
-            {/*    onClick={onToggle} */}
-            {/* > */}
-            {/*    Toggle */}
-            {/* </button> */}
+            <Button
+                data-testid="sidebar-toggle"
+                type="button"
+                onClick={onToggle}
+            >
+                {t('Переключить')}
+            </Button>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher className="" />
